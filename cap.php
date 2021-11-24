5163d5b1acc1266264c3fa4913b1a018c15adc5c
omar saade <omarsaadeh11@gmail.com>
2021-11-25 01:14:28 +0200

    scaling down images

--- cap.php
+++ cap.php
@@ -1,5 +1,23 @@
 <?php
 session_start();
-$_SESSION['secure'] = rand(1000,9999);
+
+if (!isset($_POST['secure'])) {
+    $_SESSION['secure'] = rand(1000,9999);
+} else {
+    if ($_SESSION['secure']==$_POST['secure']) {
+        echo 'A match';
+    } else {
+        echo 'Incorect try , again.';
+        $_SESSION['secure'] = rand(1000 , 9999);
+    }
+
+}
+
 ?>
-<img src="captcha.php"/>
+<br>
+<img src="captcha.php"/> <br>
+ <form action="cap.php" method="post">
+Type the value you see : <input type="text" name="secure" size="6"> <input type="submit" value="Submit">
+
+
+ </form>
--- captcha.php
+++ captcha.php
@@ -15,6 +15,16 @@ $image = imagecreate($image_width , $image_height);
 imagecolorallocate($image, 0 , 0, 0);
 $text_color = imagecolorallocate($image , 255 , 255 ,255);
 
+for ($x=1 ; $x<=30 ; $x++) {
+ $x1 = rand(1,100);
+ $y1 = rand(1,100);
+ $x2 = rand(1,100);
+ $y2 = rand(1,100);
+ imageline($image , $x1 , $y1 , $x2 , $y2 , $text_color);
+
+
+}
+
 imagettftext($image , $font_size, 0 , 0 , $font_size , $text_color, 'calliehandfullversion.ttf' ,$text);
 imagejpeg($image);
 ?>
